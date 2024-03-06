09717b0db75ba7aafab2bcfa3a58c4ddfc901c3b
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-03-06 22:08:03 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/app/Models/Client.php
+++ ERP-Botequim_Mauro/app/Models/Client.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    protected $table ='Clients';
+
+    protected $fillable =[
+        'Name_client','Surname','Age','Household'
+    ];
 }
