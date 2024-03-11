cdd225d712f21b133f712fe397250d3c94c8061e
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-03-11 12:29:45 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/database/migrations/2024_03_06_195156_create_products_table.php
+++ ERP-Botequim_Mauro/database/migrations/2024_03_06_195156_create_products_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('Product_name');
+            $table->string('Price');
+            $table->string('Quantity');
+            $table->string('Barcode');
+            $table->string('Sale_price');
+            $table->date('Expiry_date');
+            $table->date('Entry_date');
+
+            //Inicio da coluna contendo a chave estrangeiraa
+            $table->integer('Id_user')->unsigned();
+            $table->foreign('Id_user')->references('id')->on('users');
+
             $table->timestamps();
         });
     }
