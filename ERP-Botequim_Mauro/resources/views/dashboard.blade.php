55b98f0470f22cd65c8c5e54fcf1414e58bc74ed
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-03-11 12:16:27 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/resources/views/dashboard.blade.php
+++ ERP-Botequim_Mauro/resources/views/dashboard.blade.php
@@ -58,10 +58,6 @@
 <body>
     <!-- .app -->
     <div class="app">
-        <!--[if lt IE 10]>
-      <div class="page-message" role="alert">You are using an <strong>outdated</strong> browser. Please <a class="alert-link" href="http://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</div>
-      <![endif]-->
-        <!-- .app-header -->
         <header class="app-header app-header-dark">
             <!-- .top-bar -->
             <div class="top-bar">
