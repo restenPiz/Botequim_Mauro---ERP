f1d698279d4c77168289a4f730218773b1ce6e2d
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-03-11 10:09:56 +0200

    Some updates in the laratrust seeder

--- ERP-Botequim_Mauro/config/laratrust_seeder.php
+++ ERP-Botequim_Mauro/config/laratrust_seeder.php
@@ -1,31 +1,19 @@
 <?php
 
 return [
-    /**
-     * Control if the seeder should create a user per role while seeding the data.
-     */
     'create_users' => false,
 
-    /**
-     * Control if all the laratrust tables should be truncated before running the seeder.
-     */
     'truncate_tables' => true,
 
     'roles_structure' => [
-        'superadministrator' => [
+        'admin' => [
             'users' => 'c,r,u,d',
             'payments' => 'c,r,u,d',
             'profile' => 'r,u',
         ],
-        'administrator' => [
+        'user' => [
             'users' => 'c,r,u,d',
             'profile' => 'r,u',
-        ],
-        'user' => [
-            'profile' => 'r,u',
-        ],
-        'role_name' => [
-            'module_1_name' => 'c,r,u,d',
         ],
     ],
 
