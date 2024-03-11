a2c6fb3cf10b3865ed520ddce9a4ec244ee9ad0f
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-03-11 13:01:58 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/app/Models/Request.php
+++ ERP-Botequim_Mauro/app/Models/Request.php
@@ -13,4 +13,15 @@ class Request extends Model
 
     protected $fillable=['Product_name','Product_price','Quantity','Request_time',
     'Request_date','Id_client','Id_paid_credit'];
+
+    //*Inicio dos relacionamentos das chaves estrangeiras
+    public function clients()
+    {
+        return $this->hasMany(Client::class);   
+    }
+    public function paid_credits()
+    {
+        return $this->hasMany(Paid_Credit::class);   
+    }
+
 }
