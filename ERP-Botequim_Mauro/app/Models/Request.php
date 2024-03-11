2ae9a1fd7210d30b3af5aac69d6cc40dd24b21ae
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-03-11 12:59:58 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/app/Models/Request.php
+++ ERP-Botequim_Mauro/app/Models/Request.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Request extends Model
 {
     use HasFactory;
+
+    protected $table='requests';
+
+    protected $fillable=['Product_name','Product_price','Quantity','Request_time',
+    'Request_date','Id_client','Id_paid_credit'];
 }
