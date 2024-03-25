bc0546852fbe892a64a2a7bfa5ec630282272563
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-03-25 20:43:35 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/database/migrations/2024_03_06_195341_create_payments_table.php
+++ ERP-Botequim_Mauro/database/migrations/2024_03_06_195341_create_payments_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->string('Name_payment');
+            $table->string('Code');
             $table->timestamps();
         });
     }
