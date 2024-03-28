583cde7925f64773209bcf1812dab91587f81474
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-03-28 23:24:12 +0200

    Some updates in the application

---
+++ ERP-Botequim_Mauro/database/migrations/2024_03_28_212229_debit_foreign.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('debits', function (Blueprint $table) {
+            $table->unsignedBigInteger('Id_client')->unsigned();
+            $table->foreign('Id_client')->references('id')->on('clients');
+
+            $table->unsignedBigInteger('Id_request')->unsigned();
+            $table->foreign('Id_request')->references('id')->on('requests');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('debits', function (Blueprint $table) {
+            
+        });
+    }
+};
