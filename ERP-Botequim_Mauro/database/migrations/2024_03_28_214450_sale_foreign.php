5c085b4ae8d16c15c42c1c6f2475b9ebd8e0ac44
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-03-28 23:45:41 +0200

    Some updates in the application

---
+++ ERP-Botequim_Mauro/database/migrations/2024_03_28_214450_sale_foreign.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('saless', function (Blueprint $table) {
+            $table->unsignedBigInteger('Id_stock')->unsigned();
+            $table->foreign('Id_stock')->references('id')->on('stocks');
+
+            $table->unsignedBigInteger('Id_payment')->unsigned();
+            $table->foreign('Id_payment')->references('id')->on('payments');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('saless', function (Blueprint $table) {
+            //
+        });
+    }
+};
