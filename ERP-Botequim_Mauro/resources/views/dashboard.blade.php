95ca0064bcd121c2ae8a0a5f3e18796a27ec4980
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-03-29 12:40:37 +0200

    Change the language in topBar

--- ERP-Botequim_Mauro/resources/views/dashboard.blade.php
+++ ERP-Botequim_Mauro/resources/views/dashboard.blade.php
@@ -91,87 +91,8 @@
                             aria-label="toggle menu"><span class="hamburger-box"><span
                                     class="hamburger-inner"></span></span></button> <!-- /toggle menu -->
                     </div><!-- /.top-bar-item -->
-                    <!-- .top-bar-item -->
-                    <div class="top-bar-item top-bar-item-full">
-                        <!-- .top-bar-search -->
-                        <form class="top-bar-search">
-                            <!-- .input-group -->
-                            {{--<div class="input-group input-group-search dropdown">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text"><span class="oi oi-magnifying-glass"></span></span>
-                                </div><input type="text" class="form-control" data-toggle="dropdown"
-                                    aria-label="Search" placeholder="Search"> <!-- .dropdown-menu 
-                                <div class="dropdown-menu dropdown-menu-rich dropdown-menu-xl ml-n4 mw-100">
-                                    <div class="dropdown-arrow ml-3"></div><!-- .dropdown-scroll -->
-                                    <div class="dropdown-scroll perfect-scrollbar h-auto" style="max-height: 360px">
-                                        <!-- .list-group -->
-                                        <div class="list-group list-group-flush list-group-reflow mb-2">
-                                            <h6 class="list-group-header d-flex justify-content-between">
-                                                <span>Shortcut</span>
-                                            </h6><!-- .list-group-item -->
-                                            <div class="list-group-item py-2">
-                                                <a href="#" class="stretched-link"></a>
-                                                <div class="tile tile-sm bg-muted">
-                                                    <i class="fas fa-user-plus"></i>
-                                                </div>
-                                                <div class="ml-2"> Create user </div>
-                                            </div><!-- /.list-group-item -->
-                                            <!-- .list-group-item -->
-                                            <div class="list-group-item py-2">
-                                                <a href="#" class="stretched-link"></a>
-                                                <div class="tile tile-sm bg-muted">
-                                                    <i class="fas fa-dollar-sign"></i>
-                                                </div>
-                                                <div class="ml-2"> Create invoice </div>
-                                            </div><!-- /.list-group-item -->
-                                            <h6 class="list-group-header d-flex justify-content-between mt-2">
-                                                <span>In projects</span> <a href="#"
-                                                    class="font-weight-normal">Show more</a>
-                                            </h6><!-- .list-group-item -->
-                                            <div class="list-group-item py-2">
-                                                <a href="#" class="stretched-link"><span class="sr-only">Go to
-                                                        search result</span></a>
-                                                <div class="user-avatar user-avatar-sm bg-muted">
-                                                    <img src="assets/images/avatars/bootstrap.svg" alt="">
-                                                </div>
-                                                <div class="ml-2">
-                                                    <div class="mb-n1"> Bootstrap </div><small
-                                                        class="text-muted">Just now</small>
-                                                </div>
-                                            </div><!-- /.list-group-item -->
-                                            <!-- .list-group-item -->
-                                            <div class="list-group-item py-2">
-                                                <a href="#" class="stretched-link"><span class="sr-only">Go to
-                                                        search result</span></a>
-                                                <div class="user-avatar user-avatar-sm bg-muted">
-                                                    <img src="assets/images/avatars/slack.svg" alt="">
-                                                </div>
-                                                <div class="ml-2">
-                                                    <div class="mb-n1"> Slack </div><small class="text-muted">Updated
-                                                        25 minutes ago</small>
-                                                </div>
-                                            </div><!-- /.list-group-item -->
-                                            <!-- /.list-group-item -->
-                                            <h6 class="list-group-header d-flex justify-content-between mt-2">
-                                                <span>Another section</span> <a href="#"
-                                                    class="font-weight-normal">Show more</a>
-                                            </h6><!-- .list-group-item -->
-                                            <div class="list-group-item py-2">
-                                                <a href="#" class="stretched-link"><span class="sr-only">Go to
-                                                        search result</span></a>
-                                                <div class="tile tile-sm bg-muted"> P </div>
-                                                <div class="ml-2">
-                                                    <div class="mb-n1"> Product name </div>
-                                                </div>
-                                            </div><!-- /.list-group-item -->
-                                        </div><!-- /.list-group -->
-                                    </div><!-- /.dropdown-scroll -->
-                                    <a href="#" class="dropdown-footer">Show all results</a>
-                                </div><!-- /.dropdown-menu -->
-                            </div><!-- /.input-group -->--}}
-                        </form><!-- /.top-bar-search -->
-                    </div><!-- /.top-bar-item -->
-                    <!-- .top-bar-item -->
+
+                    {{--Inicio da classe dos icones acima--}}
                     <div class="top-bar-item top-bar-item-right px-0 d-none d-sm-flex">
                         <!-- .nav -->
                         <ul class="header-nav nav">
@@ -392,12 +313,10 @@
                                 <div class="dropdown-arrow ml-3 d-none d-lg-block"></div>
                                 <h6 class="dropdown-header d-none d-md-block d-lg-none"> Beni Arisandi </h6><a
                                     class="dropdown-item" href="user-profile.html"><span
-                                        class="dropdown-icon oi oi-person"></span> Profile</a> <a
+                                        class="dropdown-icon oi oi-person"></span> Perfil</a> <a
                                     class="dropdown-item" href="auth-signin-v1.html"><span
-                                        class="dropdown-icon oi oi-account-logout"></span> Logout</a>
-                                <div class="dropdown-divider"></div><a class="dropdown-item" href="#">Help
-                                    Center</a> <a class="dropdown-item" href="#">Ask Forum</a> <a
-                                    class="dropdown-item" href="#">Keyboard Shortcuts</a>
+                                        class="dropdown-icon oi oi-account-logout"></span> Sair do Sistema</a>
+                                <div class="dropdown-divider"></div><a class="dropdown-item" href="#">Central de Ajuda</a> 
                             </div><!-- /.dropdown-menu -->
                         </div><!-- /.btn-account -->
                     </div><!-- /.top-bar-item -->
