65f19707c3b6f53dfac527b6fb23447c561cd54f
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-03-31 11:31:54 +0200

    Some updates in the application

---
+++ ERP-Botequim_Mauro/resources/views/Main/Admin/addUser.blade.php
@@ -0,0 +1,8 @@
+@extends('Main.Layout.topBar')
+@section('content')
+
+    {{--Inicio da parte contendo o conteudo dos usuarios--}}
+
+    {{--Fim do conteudo da parte de adicao de usuarios--}}
+
+@endsection
