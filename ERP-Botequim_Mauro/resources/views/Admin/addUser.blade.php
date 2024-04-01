b9c74cbce5aef5930b20770370cf01fecec9845c
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-01 21:18:16 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/resources/views/Admin/addUser.blade.php
+++ ERP-Botequim_Mauro/resources/views/Admin/addUser.blade.php
@@ -1,8 +1,61 @@
 @extends('Layout.top')
 @section('content')
+    {{-- Inicio da parte contendo o conteudo dos usuarios --}}
+    <main class="app-main">
+        <!-- .wrapper -->
+        <div class="wrapper">
+            <!-- .page -->
+            <div class="page-section"></br>
+                <!-- grid row -->
+                <div class="row">
+                    <!-- grid column -->
+                    <div class="col-lg-12">
+                        <div class="col">
+                            <!-- .card -->
+                            <div class="card card-fluid">
+                                <h6 class="card-header"> Adicionar Usuarios </h6><!-- .card-body -->
+                                <div class="card-body">
+                                    <!-- form -->
+                                    <form method="post">
+                                        <!-- form row -->
+                                        <div class="form-row">
+                                            <!-- form column -->
+                                            <div class="col-md-6 mb-3">
+                                                <label for="input01">Nome</label> <input type="text"
+                                                    class="form-control" id="input01" value="Beni" required="">
+                                            </div><!-- /form column -->
+                                            <!-- form column -->
+                                            <div class="col-md-6 mb-3">
+                                                <label for="input02">Apelido</label> <input type="text"
+                                                    class="form-control" id="input02" value="Arisandi" required="">
+                                            </div><!-- /form column -->
+                                        </div><!-- /form row -->
+                                        <!-- .form-group -->
+                                        <div class="form-group">
+                                            <label for="input03">Email</label> <input type="email" class="form-control"
+                                                id="input03" value="bent10@looper.com" required="">
+                                        </div><!-- /.form-group -->
+                                        <!-- .form-group -->
+                                        <div class="form-group">
+                                            <label for="input04">Senha</label> <input type="password"
+                                                class="form-control" id="input04" value="secret" required="">
+                                        </div><!-- /.form-group -->
+                                        <hr>
+                                        <!-- .form-actions -->
+                                        <div class="form-actions">
+                                            <!-- enable submit btn when user type their current password -->
+                                            <input type="password" class="form-control mr-3" id="input06"
+                                                placeholder="Digite novamente a senha" required=""> <button type="submit"
+                                                class="btn btn-primary text-nowrap ml-auto">Adicionar Usuario</button>
+                                        </div><!-- /.form-actions -->
+                                    </form><!-- /form -->
+                                </div><!-- /.card-body -->
+                            </div><!-- /.card -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-    {{--Inicio da parte contendo o conteudo dos usuarios--}}
-
-    {{--Fim do conteudo da parte de adicao de usuarios--}}
-
-@endsection
+        {{-- Fim do conteudo da parte de adicao de usuarios --}}
+    @endsection
