65b91e9349437277f705a533cba5a52880896f3b
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-01 21:44:35 +0200

    First method completed

--- ERP-Botequim_Mauro/app/Http/Controllers/userController.php
+++ ERP-Botequim_Mauro/app/Http/Controllers/userController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use RealRashid\SweetAlert\Facades\Alert;
+use Illuminate\Support\Facades\Hash;
 
 class userController extends Controller
 {
@@ -14,6 +18,33 @@ class userController extends Controller
     public function allUser()
     {
         return view('Admin.allUser');
+    }
+    public function storeUser(Request $request)
+    {
+        if(Auth::user()->hasRole('admin'))
+        {
+            if(Request::input('User_type') == 'Attendant')
+            {
+                $user = User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'surname' => $request->surname,
+                    'User_type' => $request->userType,
+                    'password' => Hash::make($request->password),
+                ]);
+    
+                //*Atribuindo a role para o pastor quando for adicionado
+                $user->attachRole('attendant');
+    
+                Alert::success('Adicionado', 'O usuario atendente foi adicionado com sucesso!');
+    
+                return redirect()->back();
+            }
+        }
+        else
+        {
+            return redirect()->route('login');
+        }
     }
     public function updateUser($id)
     {
--- ERP-Botequim_Mauro/resources/views/Admin/addUser.blade.php
+++ ERP-Botequim_Mauro/resources/views/Admin/addUser.blade.php
@@ -23,40 +23,40 @@
                                             <!-- form column -->
                                             <div class="col-md-6 mb-3">
                                                 <label for="input01">Nome</label> <input type="text"
-                                                    class="form-control" id="input01" value="Beni" required="">
+                                                    class="form-control" id="input01" value="Beni" name="Name" required="">
                                             </div><!-- /form column -->
                                             <!-- form column -->
                                             <div class="col-md-6 mb-3">
                                                 <label for="input02">Apelido</label> <input type="text"
-                                                    class="form-control" id="input02" value="Arisandi" required="">
+                                                    class="form-control" id="input02" value="Arisandi" name="Surname" required="">
                                             </div><!-- /form column -->
                                         </div><!-- /form row -->
                                         {{--Inicio do input type select--}}
                                         <div class="form-group">
                                             <label>Tipo de Usuario</label>
-                                            <select class="form-control">
-                                                <option value="Atendente">Atendente</option>
-                                                <option value="Gestor de stock">Gestor de Stock</option>
-                                                <option value="Contabilista">Contabilista</option>
+                                            <select class="form-control" name="User_type">
+                                                <option value="Attendant">Atendente</option>
+                                                <option value="Stock_manager">Gestor de Stock</option>
+                                                <option value="Accountant">Contabilista</option>
                                             </select>
                                         </div>
                                         {{--Fim do input type select--}}
                                         <!-- .form-group -->
                                         <div class="form-group">
                                             <label for="input03">Email</label> <input type="email" class="form-control"
-                                                id="input03" value="bent10@looper.com" required="">
+                                                id="input03" value="bent10@looper.com" name="Email" required="">
                                         </div><!-- /.form-group -->
                                         <!-- .form-group -->
                                         <div class="form-group">
                                             <label for="input04">Senha</label> <input type="password"
-                                                class="form-control" id="input04" value="secret" required="">
+                                                class="form-control" id="input04" value="secret" name="Password" required="">
                                         </div><!-- /.form-group -->
                                         <hr>
                                         <!-- .form-actions -->
                                         <div class="form-actions">
                                             <!-- enable submit btn when user type their current password -->
                                             <input type="password" class="form-control mr-3" id="input06"
-                                                placeholder="Digite novamente a senha" required=""> <button type="submit"
+                                                placeholder="Digite novamente a senha" name="Password_confirmation" required=""> <button type="submit"
                                                name="submit" class="btn btn-primary text-nowrap ml-auto">Adicionar Usuario</button>
                                         </div><!-- /.form-actions -->
                                     </form><!-- /form -->
