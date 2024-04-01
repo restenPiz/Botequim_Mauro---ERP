0463a6e610883a54473ef4e653dea7f9a66732f0
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-01 21:53:29 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/resources/views/Admin/addUser.blade.php
+++ ERP-Botequim_Mauro/resources/views/Admin/addUser.blade.php
@@ -23,7 +23,7 @@
                                             <!-- form column -->
                                             <div class="col-md-6 mb-3">
                                                 <label for="input01">Nome</label> <input type="text"
-                                                    class="form-control" id="input01" value="Beni" name="Name" required="">
+                                                    class="form-control" id="input01" value="Beni" name="name" required="">
                                             </div><!-- /form column -->
                                             <!-- form column -->
                                             <div class="col-md-6 mb-3">
@@ -44,19 +44,19 @@
                                         <!-- .form-group -->
                                         <div class="form-group">
                                             <label for="input03">Email</label> <input type="email" class="form-control"
-                                                id="input03" value="bent10@looper.com" name="Email" required="">
+                                                id="input03" value="bent10@looper.com" name="email" required="">
                                         </div><!-- /.form-group -->
                                         <!-- .form-group -->
                                         <div class="form-group">
                                             <label for="input04">Senha</label> <input type="password"
-                                                class="form-control" id="input04" value="secret" name="Password" required="">
+                                                class="form-control" id="input04" value="secret" name="password" required="">
                                         </div><!-- /.form-group -->
                                         <hr>
                                         <!-- .form-actions -->
                                         <div class="form-actions">
                                             <!-- enable submit btn when user type their current password -->
                                             <input type="password" class="form-control mr-3" id="input06"
-                                                placeholder="Digite novamente a senha" name="Password_confirmation" required=""> <button type="submit"
+                                                placeholder="Digite novamente a senha" name="password_confirmation" required=""> <button type="submit"
                                                name="submit" class="btn btn-primary text-nowrap ml-auto">Adicionar Usuario</button>
                                         </div><!-- /.form-actions -->
                                     </form><!-- /form -->
