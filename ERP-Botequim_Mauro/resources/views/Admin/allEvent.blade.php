d239c8358a439714ac5d88107dc09c936809c757
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-02 22:17:41 +0200

    Some updates in the application

---
+++ ERP-Botequim_Mauro/resources/views/Admin/allEvent.blade.php
@@ -0,0 +1,26 @@
+@extends('Layout.top')
+@section('content')
+    {{-- Inicio da parte contendo o conteudo dos usuarios --}}
+    <main class="app-main">
+        <!-- .wrapper -->
+        <div class="wrapper">
+            <!-- .page -->
+            <div class="page-section"></br>
+                <!-- grid row -->
+                <div class="row">
+                    <!-- grid column -->
+                    <div class="col-lg-12">
+                        <div class="col">
+
+                            {{--Inicio da tabela de todos eventos--}}
+
+                            {{--Fim da tabela de todos eventos--}}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+@endsection
