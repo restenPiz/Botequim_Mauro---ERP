310bc53319e48ee03766467f31b7451ad698a7ad
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-03 22:54:08 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/resources/views/Admin/allEvent.blade.php
+++ ERP-Botequim_Mauro/resources/views/Admin/allEvent.blade.php
@@ -86,13 +86,44 @@
                                                                     class="fa fa-pencil-alt"></i> <span
                                                                     class="sr-only">Edit</span></button> <button
                                                                 type="button" class="btn btn-sm btn-icon btn-secondary"><i
-                                                                    class="far fa-trash-alt"></i> <span
+                                                                    class="far fa-trash-alt" data-target="#deleteRecordModal{{ $event->id }}" data-toggle="modal"></i> <span
                                                                     class="sr-only">Remove</span></button>
                                                         </td>
                                                     </tr>
 
                                                     {{--Inicio do modal de eliminar--}}
-
+                                                    <div class="modal fade zoomIn" id="deleteRecordModal{{ $event->id }}"
+                                                        tabindex="-1" aria-hidden="true">
+                                                        <div class="modal-dialog modal-dialog-centered">
+                                                            <div class="modal-content">
+                                                                <form action="{{route('deleteModal',['id'=>$event->id])}}" method="get">
+                                                                    @csrf
+                                                                    @method('DELETE')
+                                                                    <div class="modal-body">
+                                                                        <div class="mt-2 text-center">
+                                                                            <lord-icon src="https://cdn.lordicon.com/gsqxdxog.json"
+                                                                                trigger="loop"
+                                                                                colors="primary:#f7b84b,secondary:#f06548"
+                                                                                style="width:100px;height:100px">
+                                                                            </lord-icon>
+                                                                            <div class="mt-4 pt-2 fs-15 mx-4 mx-sm-5">
+                                                                                <h4>Voce tem certeza ?</h4>
+                                                                                <p class="text-muted mx-4 mb-0">Voce pretende eliminar
+                                                                                 este comunicado ?</p>
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="d-flex gap-2 justify-content-center mt-4 mb-2">
+                                                                            <button type="button" class="btn w-sm btn-light"
+                                                                                data-bs-dismiss="modal">Fechar</button>
+                                                                            <button type="submit" name="submit"
+                                                                                class="btn w-sm btn-danger " id="delete-record">Sim,
+                                                                                elimine!</button>
+                                                                        </div>
+                                                                    </div>
+                                                                </form>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                     {{--Fim do modal de eliminar--}}
 
                                                     {{--Inicio do modal de edicao--}}
