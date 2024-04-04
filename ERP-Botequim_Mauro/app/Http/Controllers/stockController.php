bc8f701882210f0ab8b8e8ac9df6e0becae1f662
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-04 21:48:48 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/app/Http/Controllers/stockController.php
+++ ERP-Botequim_Mauro/app/Http/Controllers/stockController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Stock;
-use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
+use Request;
 
 class stockController extends Controller
 {
@@ -12,5 +13,57 @@ class stockController extends Controller
         $stocks = Stock::all();
         
         return view('Admin.allStock-in',compact('stocks'));
+    }
+    public function addStock()
+    {
+        return view('Admin.addStock');
+    }
+    public function updateStock($id)
+    {
+        $stock = Stock::find($id);
+
+        $stock->Product_name=Request::input('Product_name');
+        $stock->Sale=Request::input('Sale');
+        $stock->Quantity=Request::input('Quantity');
+        $stock->Barcode=Request::input('Barcode');
+        $stock->Price=Request::input('Price');
+        $stock->Entry_date=Request::input('Entry_date');
+        $stock->Expiry_date=Request::input('Expiry_date');
+        $stock->Invoice_number=Request::input('Invoice_number');
+
+        $stock->save();
+
+        Alert::success('Actualizado','O producto foi actualizado com sucesso!');
+
+        return back();
+    }
+    public function storeStock()
+    {
+        $stock=new Stock();
+
+        $stock->Product_name=Request::input('Product_name');
+        $stock->Sale=Request::input('Sale');
+        $stock->Quantity=Request::input('Quantity');
+        $stock->Barcode=Request::input('Barcode');
+        $stock->Price=Request::input('Price');
+        $stock->Entry_date=Request::input('Entry_date');
+        $stock->Expiry_date=Request::input('Expiry_date');
+        $stock->Invoice_number=Request::input('Invoice_number');
+
+        $stock->save();
+
+        Alert::success('Adicionado','O producto foi adicionado com sucesso!');
+
+        return back();
+    }
+    public function delete($id)
+    {
+        $stock=Stock::find($id);
+
+        $stock::delete();
+
+        Alert::success('Eliminado','O producto foi eliminado com sucesso!');
+
+        return back();
     }
 }
