ce14adb9428bff68e5dd7a69f3551e0869f0197b
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-04 09:45:14 +0200

    Some updates in the applicationg

--- ERP-Botequim_Mauro/app/Http/Controllers/categoriesController.php
+++ ERP-Botequim_Mauro/app/Http/Controllers/categoriesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-use Illuminate\Http\Request;
+use Request;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class categoriesController extends Controller
