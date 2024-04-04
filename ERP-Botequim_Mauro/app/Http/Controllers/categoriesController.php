22b4c78900c52868d6986efdf1402538675a9c8a
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-04 09:43:50 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/app/Http/Controllers/categoriesController.php
+++ ERP-Botequim_Mauro/app/Http/Controllers/categoriesController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class categoriesController extends Controller
 {
-    //
+    public function allCategories()
+    {
+        $categories=Category::all();
+
+        return view('Admin.allCategories',compact('categories'));
+    }
+    public function storeCategories()
+    {
+        $table=new Category();
+
+        $table->Category_name=Request::input('Category_name');
+        $table->Code=Request::input('Code');
+
+        $table->save();
+
+        Alert::success('Adicionado!','A sua categoria foi adicionada com sucesso!');
+
+        return back();
+    }
+    public function updateCategories($id)
+    {
+        $categories=Category::findOrFail($id);
+
+        $categories->Category_name=Request::input('Category_name');
+        $categories->Code=Request::input('Code');
+
+        $categories->save();
+
+        Alert::success('Adicionado!','A categoria foi adicionada com sucesso!');
+
+        return back();
+    }
+    public function deleteCategories($id)
+    {
+        $categories=Category::findOrFail($id);
+
+        $categories->delete();
+
+        return back();
+    }
 }
