bac75c2c8256205a215b0f65e6b5ee0e1b8c556a
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-05 13:46:36 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/database/migrations/2024_03_06_195415_create_stocks_table.php
+++ ERP-Botequim_Mauro/database/migrations/2024_03_06_195415_create_stocks_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('stocks_in', function (Blueprint $table) {
+        Schema::create('stocks', function (Blueprint $table) {
             $table->id();
             $table->string('Product_name');
             $table->string('Quantity');
