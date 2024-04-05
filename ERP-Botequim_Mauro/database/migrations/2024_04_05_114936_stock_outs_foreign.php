fa9ce598d502c28af388c3ec29c0dcf453b41c51
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-05 13:51:07 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/database/migrations/2024_04_05_114936_stock_outs_foreign.php
+++ ERP-Botequim_Mauro/database/migrations/2024_04_05_114936_stock_outs_foreign.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('stock_outs', function (Blueprint $table) {
-            $table->unsignedBigInteger('Id_products')->unsigned();
+            $table->unsignedBigInteger('Id_product')->unsigned();
             $table->foreign('Id_product')->references('id')->on('products');
         });
     }
