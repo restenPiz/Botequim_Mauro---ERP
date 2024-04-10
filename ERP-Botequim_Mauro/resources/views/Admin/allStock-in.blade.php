3d23ed67238401ab5e92f13fe4de383037894701
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-10 13:37:13 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/resources/views/Admin/allStock-in.blade.php
+++ ERP-Botequim_Mauro/resources/views/Admin/allStock-in.blade.php
@@ -227,7 +227,7 @@
                                                 <select class="form-control" name="Product_name">
                                                     <option>--Selecione o Producto --</option>
                                                     @foreach ($stocks as $product)
-                                                    <option value="{{$product->productos->Product_name}}">{{$product->productos->Product_name}}</option>
+                                                    <option value="{{$product->productos->id}}">{{$product->productos->id}}</option>
                                                     @endforeach
                                                 </select>   
                                             </div><!-- /form column -->
