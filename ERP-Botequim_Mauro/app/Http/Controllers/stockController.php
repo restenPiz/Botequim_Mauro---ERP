db1fb6debb7ca0f381d9cc559face57a67f9cc2a
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-12 09:28:39 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/app/Http/Controllers/stockController.php
+++ ERP-Botequim_Mauro/app/Http/Controllers/stockController.php
@@ -89,8 +89,10 @@ class stockController extends Controller
     public function getProduct()
     {
         $input= Request::input('id');
-
-        $product=Stock::where('id','=',$input)->get();
+        
+        $product = DB::table('stocks')
+            ->where('id', $input)
+            ->get();
 
         if ($product) {
             // Retorna os detalhes do produto como JSON
