49018665c5ec819804c155bc1712c98d35fcff16
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-13 23:29:40 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/resources/views/Admin/addClient.blade.php
+++ ERP-Botequim_Mauro/resources/views/Admin/addClient.blade.php
@@ -52,8 +52,94 @@
 
                     {{--Inicio da tabela de todos clientes--}}
 
+                    <div class="col-lg-8">
+                        <div class="col">
 
-                    
+                            {{-- Inicio da tabela de todos eventos --}}
+                            <header class="page-title-bar">
+                                <!-- floating action -->
+                                <button type="button" class="btn btn-success btn-floated"><span
+                                        class="fa fa-plus"></span></button> <!-- /floating action -->
+                                <!-- title and toolbar -->
+                                <div class="d-md-flex align-items-md-start">
+                                    <h1 class="page-title mr-sm-auto"> Todos Clientes </h1><!-- .btn-toolbar -->
+                                    <div class="btn-toolbar">
+                                        <button type="button" class="btn btn-light"><i
+                                                class="oi oi-data-transfer-download"></i> <span
+                                                class="ml-1">Exportar</span></button> <button type="button"
+                                            class="btn btn-light"><i class="oi oi-data-transfer-upload"></i> <span
+                                                class="ml-1">Importar</span></button>
+                                        <div class="dropdown">
+                                            <button type="button" class="btn btn-light"
+                                                data-toggle="dropdown"><span>Mais</span> <span
+                                                    class="fa fa-caret-down"></span></button>
+                                            <div class="dropdown-menu dropdown-menu-right">
+                                                <div class="dropdown-arrow"></div><a href="#"
+                                                    class="dropdown-item">Add tasks</a> <a href="#"
+                                                    class="dropdown-item">Invite members</a>
+                                                <div class="dropdown-divider"></div><a href="#"
+                                                    class="dropdown-item">Share</a> <a href="#"
+                                                    class="dropdown-item">Archive</a> <a href="#"
+                                                    class="dropdown-item">Remove</a>
+                                            </div>
+                                        </div>
+                                    </div><!-- /.btn-toolbar -->
+                                </div><!-- /title and toolbar -->
+                            </header><!-- /.page-title-bar -->
+                            <!-- .page-section -->
+
+                            {{-- Table section --}}
+                            <div class="card mt-4" style="margin-top:-4rem">
+                                <!-- .card-body -->
+                                <div class="card-body">
+                                    {{-- <h2 class="card-title"> Contacts </h2><!-- .table-responsive --> --}}
+                                    <div class="table-responsive">
+                                        <table class="table table-striped" style="min-width: 678px">
+                                            <thead>
+                                                <tr>
+                                                    <th> Nome do Cliente </th>
+                                                    <th> Apelido </th>
+                                                    <th> Idade </th>
+                                                    <th> Morada </th>
+                                                    <th></th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($clients as $client)
+                                                    <tr>
+                                                        <td class="align-middle"> {{ $client->Client_name }}</td>
+                                                        <td class="align-middle"> {{ $client->Surname }} </td>
+                                                        <td class="align-middle"> {{ $client->Age }} </td>
+                                                        <td class="align-middle"> {{ $client->Household }} </td>
+                                                        <td class="align-middle text-right">
+                                                            <button type="button" class="btn btn-sm btn-icon btn-secondary"
+                                                                data-toggle="modal" data-target="#clientNewModal{{ $client->id }}"><i
+                                                                    class="fa fa-pencil-alt"></i> <span
+                                                                    class="sr-only">Edit</span></button> <button
+                                                                type="button" class="btn btn-sm btn-icon btn-secondary"><i
+                                                                    class="far fa-trash-alt" data-target="#deleteRecordModal{{ $client->id }}" data-toggle="modal"></i> <span
+                                                                    class="sr-only">Remove</span></button>
+                                                        </td>
+                                                    </tr>
+
+                                                    {{--Inicio do modal de eliminar--}}
+
+                                                    {{--Fim do modal de eliminar--}}
+
+                                                    {{--Inicio do modal de editar--}}
+
+                                                    {{--Fim do modal de editar--}}
+
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
+
                     {{--Fim da tabela de todos clientes--}}
                 </div>
             </div>
