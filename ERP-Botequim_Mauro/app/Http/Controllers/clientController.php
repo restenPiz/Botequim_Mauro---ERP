072591f048c2e9eeec85738ef109fdc4be52dc54
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-13 23:31:04 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/app/Http/Controllers/clientController.php
+++ ERP-Botequim_Mauro/app/Http/Controllers/clientController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class clientController extends Controller
 {
     public function addClient()
     {
-        return view("Admin.addClient");
+        $clients=Client::all();
+        
+        return view("Admin.addClient",compact('clients'));
     }
 }
