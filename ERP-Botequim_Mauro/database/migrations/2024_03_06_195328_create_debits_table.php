12f16efabfe02634a5cef1e49adfa0e502881f0b
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-15 22:07:58 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/database/migrations/2024_03_06_195328_create_debits_table.php
+++ ERP-Botequim_Mauro/database/migrations/2024_03_06_195328_create_debits_table.php
@@ -13,20 +13,8 @@ return new class extends Migration
     {
         Schema::create('debits', function (Blueprint $table) {
             $table->id();
-            $table->string('Client_name');
-            $table->string('Surname');
             $table->string('Value');
-            $table->string('Bi_number');
             $table->string('Date_to_pay');
-            $table->string('Description');
-
-            //*Inicio das chaves estrangeiras
-        /*    $table->unsignedBigInteger('Id_client')->unsigned();
-            $table->foreign('Id_client')->references('id')->on('clients');
-
-            $table->unsignedBigInteger('Id_request')->unsigned();
-            $table->foreign('Id_request')->references('id')->on('requests');
-        */
             $table->timestamps();
         });
     }
