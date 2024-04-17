e3e7aae59adc446044ec7d0fbfdd576e236368ab
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-17 09:31:00 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/app/Http/Controllers/paymentController.php
+++ ERP-Botequim_Mauro/app/Http/Controllers/paymentController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
+use Request;
+use App\Models\Payment;
 
 class paymentController extends Controller
 {
-    //
+    public function addPayment()
+    {
+        return view("Admin.allPayment");
+    }
+    public function storePayment()
+    {
+        $payment=new Payment();
+
+        $payment->Name_payment=Request::input('Name_payment');
+        $payment->Code=Request::input('Code');
+
+        $payment->save();
+
+        Alert::success('Adicionado!','O tipo de pagamento foi adicionado com sucesso!');
+
+        return back();
+    }
+    public function updatePayment()
+    {
+
+    }
 }
