0753fe4b9c37b5f9dfee87ed96b2860b5f213ad8
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-18 14:53:57 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/resources/views/Admin/allDebit.blade.php
+++ ERP-Botequim_Mauro/resources/views/Admin/allDebit.blade.php
@@ -29,7 +29,7 @@
                                             </div><!-- /form column -->
                                             <div class="col-md-12 mb-3">
                                                 <label for="input01">Nome do Producto</label> 
-                                                <select class="form-control" name="Id_product" id="Id_product" onchange="prod(this);">
+                                                <select class="form-control" name="Id_stock" id="Id_product" onchange="prod(this);">
                                                     <option>--Selecione o Producto --</option>
                                                     @foreach ($products as $product)
                                                     <option value="{{$product->product->id}}">{{$product->product->Product_name}}</option>
