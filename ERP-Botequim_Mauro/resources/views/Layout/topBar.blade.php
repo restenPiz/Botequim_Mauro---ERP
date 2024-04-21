8e7097a40adcdf3123184da0b92c9f32b839de7d
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-04-21 13:05:30 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/resources/views/Layout/topBar.blade.php
+++ ERP-Botequim_Mauro/resources/views/Layout/topBar.blade.php
@@ -587,13 +587,13 @@
         <link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,500,600" rel="stylesheet">
         <!-- End GOOGLE FONT -->
         <!-- BEGIN PLUGINS STYLES -->
-        <link rel="stylesheet" href="assets/vendor/open-iconic/font/css/open-iconic-bootstrap.min.css">
-        <link rel="stylesheet" href="assets/vendor/%40fortawesome/fontawesome-free/css/all.min.css">
-        <link rel="stylesheet" href="assets/vendor/flatpickr/flatpickr.min.css"><!-- END PLUGINS STYLES -->
+        <link rel="stylesheet" href="../assets/vendor/open-iconic/font/css/open-iconic-bootstrap.min.css">
+        <link rel="stylesheet" href="../assets/vendor/%40fortawesome/fontawesome-free/css/all.min.css">
+        <link rel="stylesheet" href="../assets/vendor/flatpickr/flatpickr.min.css"><!-- END PLUGINS STYLES -->
         <!-- BEGIN THEME STYLES -->
-        <link rel="stylesheet" href="assets/stylesheets/theme.min.css" data-skin="default">
-        <link rel="stylesheet" href="assets/stylesheets/theme-dark.min.css" data-skin="dark">
-        <link rel="stylesheet" href="assets/stylesheets/custom.css">
+        <link rel="stylesheet" href="../assets/stylesheets/theme.min.css" data-skin="default">
+        <link rel="stylesheet" href="../assets/stylesheets/theme-dark.min.css" data-skin="dark">
+        <link rel="stylesheet" href="../assets/stylesheets/custom.css">
         <script>
             var skin = localStorage.getItem('skin') || 'default';
             var disabledSkinStylesheet = document.querySelector('link[data-skin]:not([data-skin="' + skin + '"])');
@@ -989,20 +989,20 @@
             }
         </script>
 
-        <script src="assets/vendor/jquery/jquery.min.js"></script>
-        <script src="assets/vendor/popper.js/umd/popper.min.js"></script>
-        <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script> <!-- END BASE JS -->
+        <script src="../assets/vendor/jquery/jquery.min.js"></script>
+        <script src="../assets/vendor/popper.js/umd/popper.min.js"></script>
+        <script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script> <!-- END BASE JS -->
         <!-- BEGIN PLUGINS JS -->
-        <script src="assets/vendor/pace-progress/pace.min.js"></script>
-        <script src="assets/vendor/stacked-menu/js/stacked-menu.min.js"></script>
-        <script src="assets/vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-        <script src="assets/vendor/flatpickr/flatpickr.min.js"></script>
-        <script src="assets/vendor/easy-pie-chart/jquery.easypiechart.min.js"></script>
-        <script src="assets/vendor/chart.js/Chart.min.js"></script> <!-- END PLUGINS JS -->
+        <script src="../assets/vendor/pace-progress/pace.min.js"></script>
+        <script src="../assets/vendor/stacked-menu/js/stacked-menu.min.js"></script>
+        <script src="../assets/vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+        <script src="../assets/vendor/flatpickr/flatpickr.min.js"></script>
+        <script src="../assets/vendor/easy-pie-chart/jquery.easypiechart.min.js"></script>
+        <script src="../assets/vendor/chart.js/Chart.min.js"></script> <!-- END PLUGINS JS -->
         <!-- BEGIN THEME JS -->
-        <script src="assets/javascript/theme.min.js"></script> <!-- END THEME JS -->
+        <script src="../assets/javascript/theme.min.js"></script> <!-- END THEME JS -->
         <!-- BEGIN PAGE LEVEL JS -->
-        <script src="assets/javascript/pages/dashboard-demo.js"></script> <!-- END PAGE LEVEL JS -->
+        <script src="./assets/javascript/pages/dashboard-demo.js"></script> <!-- END PAGE LEVEL JS -->
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116692175-1"></script>
         <script>
