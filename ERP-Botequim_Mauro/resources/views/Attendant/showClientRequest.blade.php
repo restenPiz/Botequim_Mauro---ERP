b1463b368db9e3a719825f022e65559d43414f33
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-05-06 21:33:54 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/resources/views/Attendant/showClientRequest.blade.php
+++ ERP-Botequim_Mauro/resources/views/Attendant/showClientRequest.blade.php
@@ -31,7 +31,7 @@
                                             <div class="col-md-12 mb-3">
                                                 <label for="input01">Nome do Producto</label>
                                                 <select class="form-control" name="Id_stock" id="Id_product"
-                                                    onchange="prod(this);">
+                                                    onchange="productos(this);">
                                                     <option>--Selecione o Producto --</option>
                                                     @foreach ($stocks as $product)
                                                         <option value="{{ $product->product->id }}">
