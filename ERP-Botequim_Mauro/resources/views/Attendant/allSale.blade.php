13cca334e6d569af748669c7bd19354e5d3ba143
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-05-17 13:08:59 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/resources/views/Attendant/allSale.blade.php
+++ ERP-Botequim_Mauro/resources/views/Attendant/allSale.blade.php
@@ -1,6 +1,11 @@
 @extends('Layout.topBar')
 @section('content')
-    {{-- Inicio da parte de todas as vendas--}}
+
+    {{--Inicio da parte de todas vendas--}}
+
+    @role('attendant')
+
+    {{--Inicio da parte do atendente--}}
     <main class="app-main">
         <!-- .wrapper -->
         <div class="wrapper">
@@ -111,4 +116,125 @@
                 </div>
             </div>
     </main>
+
+    @endrole
+
+    @role('admin')
+
+    {{--Inicio da parte de administrador--}}
+    <main class="app-main">
+        <!-- .wrapper -->
+        <div class="wrapper">
+            <!-- .page -->
+            <div class="page-section"></br>
+                <!-- grid row -->
+                <div class="row">
+                    <!-- grid column -->
+                    <div class="col-lg-12">
+                        <div class="col">
+
+                            {{-- Inicio da tabela de todos eventos --}}
+                            <header class="page-title-bar"><!-- /.breadcrumb -->
+                                <!-- floating action -->
+                                <button type="button" class="btn btn-success btn-floated"><span
+                                        class="fa fa-plus"></span></button> <!-- /floating action -->
+                                <!-- title and toolbar -->
+                                <div class="d-md-flex align-items-md-start">
+                                    <h1 class="page-title mr-sm-auto"> Todas Vendas </h1><!-- .btn-toolbar -->
+                                    <div class="btn-toolbar">
+                                        <button type="button" class="btn btn-light"><i
+                                                class="oi oi-data-transfer-download"></i> <span
+                                                class="ml-1">Exportar</span></button> 
+                                    </div><!-- /.btn-toolbar -->
+                                </div><!-- /title and toolbar -->
+                            </header><!-- /.page-title-bar -->
+                            <!-- .page-section -->
+
+                            {{-- Table section --}}
+                            <div class="card mt-4" style="margin-top:-4rem">
+                                <!-- .card-body -->
+                                <div class="card-body">
+                                    {{-- <h2 class="card-title"> Contacts </h2><!-- .table-responsive --> --}}
+                                    <div class="table-responsive">
+
+                                        <div class="form-group">
+                                            <!-- .input-group -->
+                                            <div class="input-group input-group-alt">
+                                              <!-- .input-group-prepend -->
+                                              <div class="input-group-prepend">
+                                                <select id="filterBy" class="custom-select">
+                                                  <option value='' selected> Filtrar Por </option>
+                                                </select>
+                                              </div><!-- /.input-group-prepend -->
+                                              <!-- .input-group -->
+                                              <div class="input-group has-clearable">
+                                                <button id="clear-search" type="button" class="close" aria-label="Close"><span aria-hidden="true"><i class="fa fa-times-circle"></i></span></button>
+                                                <div class="input-group-prepend">
+                                                  <span class="input-group-text"><span class="oi oi-magnifying-glass"></span></span>
+                                                </div><input id="table-search" type="text" class="form-control" placeholder="Pesquisar productos">
+                                              </div><!-- /.input-group -->
+                                            </div><!-- /.input-group -->
+                                          </div><!-- /.form-group -->
+
+                                        <table class="table table-striped" style="min-width: 678px">
+                                            <thead>
+                                                <tr>
+                                                    <th> Nome do Producto </th>
+                                                    <th> Quantidade </th>
+                                                    <th> Metodo de Pagamento </th>
+                                                    <th> Preco de Venda </th>
+                                                    <th> Valor Total </th>
+                                                    <th> Valor Pago </th>
+                                                    <th> Troco </th>
+                                                    <th> IVA </th>
+                                                    <th> Data de Venda </th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($products as $product)
+                                                    <tr>
+                                                        <td class="align-middle"> {{ $product->stocks->product->Product_name }} </td>
+                                                        <td class="align-middle"> {{ $product->Quantity }} </td>
+                                                        <td class="align-middle"> 
+                                                            <span class="badge badge-subtle badge-success">{{$product->payments->Name_payment}}</span>
+                                                        </td>
+                                                        <td class="align-middle"> {{ $product->Product_price }} </td>
+                                                        <td class="align-middle"> {{ $product->Amount }} </td>
+                                                        <td class="align-middle"> 
+                                                            <span class="badge badge-subtle badge-primary">{{ $product->Total_price }}
+                                                            </span></td>
+                                                        <td class="align-middle"> {{
+                                                            
+                                                            $troco=$product->Total_price - $product->Amount
+                                                            
+                                                        }} </td>
+                                                        <td class="align-middle"> {{ $iva }} </td>
+                                                        <td class="align-middle"> 
+                                                            <span class="badge badge-subtle badge-warning">{{$product->created_at}}</span>
+                                                        </td>
+                                                    </tr>
+
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div><!-- /.table-responsive -->
+                                </div><!-- /.card-body -->
+                                <!-- .card-footer -->
+                                <div class="card-footer">
+                                    <a href="{{route('dashboard')}}" class="card-footer-item"><i
+                                            class="fa fa-plus-circle mr-1"></i> Adicionar Venda</a>
+                                </div><!-- /.card-footer -->
+                            </div>
+                            {{-- End of table section --}}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </main>
+
+    @endrole
+
+    {{--Fim da parte de todas vendas--}}
+
 @endsection
