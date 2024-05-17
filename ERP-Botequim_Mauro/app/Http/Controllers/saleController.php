35cb7948fa3b674dbea10836a173c16a555963e0
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-05-17 09:04:45 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/app/Http/Controllers/saleController.php
+++ ERP-Botequim_Mauro/app/Http/Controllers/saleController.php
@@ -55,16 +55,50 @@ class saleController extends Controller
 
     public function storeSaleHistory()
     {
-        $sales = Sale::all();
+        // $sales = Sale::all();
+
+        // // Iterar sobre as vendas e mover os dados para a tabela sale_histories
+        // foreach ($sales as $sale) {
+        //     Sale_History::create([
+        //         'Product_price' => $sale->Product_price, // calcular o total_price
+        //         'Quantity' => $sale->Quantity,
+        //         'Id_stock' => $sale->Id_stock,
+        //         'Amount'=> $sale->Product_price * $sale->Quantity,
+        //         'Total_price'=>Request::input('Total_price'),
+        //         'Id_payment'=>Request::input('Id_payment'),
+        //     ]);
+        // }
+
+        // // Deletar os dados da tabela sales
+        // Sale::truncate();
+
+        // Alert::success('Vendido','O producto foi vendido com sucesso!');
+
+        // return back();
+
+    // Calcular o preço total da venda com base nos produtos vendidos
+    $totalPrice = Sale::sum('Product_price');
+
+        // Obter o valor pago pelo cliente (Total_price)
+        $valorPago = Request::input('Total_price');
+
+        // Calcular o valor do IVA (por exemplo, 20%)
+        $iva = $totalPrice * 0.20;
+
+        // Calcular o troco
+        $troco = $valorPago - ($totalPrice + $iva);
 
         // Iterar sobre as vendas e mover os dados para a tabela sale_histories
+        $sales = Sale::all();
         foreach ($sales as $sale) {
             Sale_History::create([
-                'Product_price' => $sale->Product_price, // calcular o total_price
+                'Product_price' => $sale->Product_price,
                 'Quantity' => $sale->Quantity,
                 'Id_stock' => $sale->Id_stock,
                 'Amount'=> $sale->Product_price * $sale->Quantity,
-                'Total_price'=>Request::input('Total_price'),
+                'Total_price'=> $totalPrice,
+                'IVA' => $iva,
+                'Troco' => $troco,
                 'Id_payment'=>Request::input('Id_payment'),
             ]);
         }
@@ -72,9 +106,10 @@ class saleController extends Controller
         // Deletar os dados da tabela sales
         Sale::truncate();
 
-        Alert::success('Vendido','O producto foi vendido com sucesso!');
+        Alert::success('Vendido','O produto foi vendido com sucesso!');
 
         return back();
+
     }
     //?Fim dos metodos de conclusao de venda
 
