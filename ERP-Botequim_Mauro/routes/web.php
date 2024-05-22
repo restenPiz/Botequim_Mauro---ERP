2ad8ea9235e556b9371da9f0e865eb3e68604ec1
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-05-22 11:30:44 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/routes/web.php
+++ ERP-Botequim_Mauro/routes/web.php
@@ -107,6 +107,7 @@ Route::group(['prefix' => 'attendant', 'middleware' => ['role:attendant']], func
     Route::get('/deleteAllSale', [saleController::class, 'deleteAllSale'])->name('deleteAllSale');
     //*Inicio da rota de insercao na tabela de vendas
     Route::post('/storeSaleHistory', [saleController::class, 'storeSaleHistory'])->name('storeSaleHistory');
+    Route::post('/storeSaleRequest', [saleController::class, 'storeSaleRequest'])->name('storeSaleRequest');
     //*Fim das rotas de insercao na tabela de vendas
 
 });
