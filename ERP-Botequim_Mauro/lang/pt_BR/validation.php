294442a4ac5b20e5615e04c6dd73614b1bdc6fd2
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-06-06 10:08:49 +0200

    Some updates in the application

--- ERP-Botequim_Mauro/lang/pt_BR/validation.php
+++ ERP-Botequim_Mauro/lang/pt_BR/validation.php
@@ -2,17 +2,6 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Validation Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines contain the default error messages used by
-    | the validator class. Some of these rules have multiple versions such
-    | as the size rules. Feel free to tweak each of these messages here.
-    |
-    */
-
     'accepted'             => 'O campo :attribute deve ser aceito.',
     'accepted_if'          => 'O :attribute deve ser aceito quando :other for :value.',
     'active_url'           => 'O campo :attribute não é uma URL válida.',
@@ -140,35 +129,17 @@ return [
     'url'                  => 'O campo :attribute tem um formato inválido.',
     'uuid' => 'O campo :attribute deve ser um UUID válido.',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Custom Validation Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify custom validation messages for attributes using the
-    | convention "attribute.rule" to name the lines. This makes it quick to
-    | specify a specific custom language line for a given attribute rule.
-    |
-    */
-
     'custom' => [
         'attribute-name' => [
             'rule-name' => 'custom-message',
         ],
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Custom Validation Attributes
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used to swap our attribute placeholder
-    | with something more reader friendly such as "E-Mail Address" instead
-    | of "email". This simply helps us make our message more expressive.
-    |
-    */
-
     'attributes' => [
+        'name' => 'nome',
+        'Surname' => 'apelido',
+        'email' => 'email',
+        'password' => 'senha',
         'address'   => 'endereço',
         'age'       => 'idade',
         'body'      => 'conteúdo',
@@ -189,12 +160,10 @@ return [
         'minute'    => 'minuto',
         'mobile'    => 'celular',
         'month'     => 'mês',
-        'name'      => 'nome',
         'zipcode'   => 'cep',
         'company_name'   => 'razão social',
         'neighborhood' => 'bairro',
         'number'    => 'número',
-        'password'  => 'senha',
         'phone'     => 'telefone',
         'second'    => 'segundo',
         'sex'       => 'sexo',
