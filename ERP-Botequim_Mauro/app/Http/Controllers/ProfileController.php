81c4d2e2cbb724c83aef825a98fd31341a842014
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-06-17 21:47:14 +0200

    Some updates in the applicationb

--- ERP-Botequim_Mauro/app/Http/Controllers/ProfileController.php
+++ ERP-Botequim_Mauro/app/Http/Controllers/ProfileController.php
@@ -3,17 +3,58 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\View\View;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class ProfileController extends Controller
 {
-    /**
-     * Display the user's profile form.
-     */
+    //*Inicio dos metodos de exibicao e edicao dos dados do perfil
+    public function updateProfile($id)
+    {
+        $user=User::findOrFail($id);
+
+        return view('userProfile',compact('user'));
+    }
+
+    public function storeProfile(Request $request, $id)
+    {
+        $validator = Validator::make(Request::all(), [
+            'name' => 'required|string|max:255',
+            'Surname' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $id,
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        //*Inicio do metodo responsavel por redirecionar com o erro
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator, 'updateUser')
+                ->withInput($request->all())
+                ->with(['modal_id' => $id]);
+        }
+
+        $user = User::findOrFail($id);
+        $user->name = Request::input('name');
+        $user->Surname = Request::input('Surname');
+        $user->email = Request::input('email');
+
+        if (Request::filled('password')) {
+            $user->password = bcrypt(Request::input('password'));
+        }
+
+        $user->save();
+
+        Alert::success('Actualizado','Os dados do usuario foram actualizados com sucesso!');
+
+        return redirect()->back();
+    }
+
     public function edit(Request $request): View
     {
         return view('profile.edit', [
