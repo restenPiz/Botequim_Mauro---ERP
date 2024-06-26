b0fbb1a63282174cf66f7b2d7f1adc0d92ef8346
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-06-26 13:32:25 +0200

    Some updates in the application

---
+++ ERP-Botequim_Mauro/resources/views/pdf/debit.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Debts</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <h1>Debts of {{ $client->Name_client }} {{ $client->Surname }}</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Nome de Producto</th>
+                <th>Preço</th>
+                <th>Data de Pagamento</th>
+                <th>Quantidade</th>
+                <th>Valor Total</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($debits as $debit)
+                <tr>
+                    <td>{{ $debit->product->product->Product_name }}</td>
+                    <td>{{ number_format($debit->Product_price, 2, ',', '.') }} MZN</td>
+                    <td>{{ $debit->Date_to_pay }}</td>
+                    <td>{{ $debit->Quantity }}</td>
+                    <td>{{ number_format($debit->Amount, 2, ',', '.') }} MZN</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
