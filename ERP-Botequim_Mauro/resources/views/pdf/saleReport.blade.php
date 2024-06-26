b29a42b2c66eed113f687c3d903f00a3e237fb20
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-06-26 12:24:55 +0200

    SOme updates in the application

--- ERP-Botequim_Mauro/resources/views/pdf/saleReport.blade.php
+++ ERP-Botequim_Mauro/resources/views/pdf/saleReport.blade.php
@@ -17,9 +17,6 @@
         <h2>Gráfico sobre as quantidades dos produtos</h2>
         <img src="{{ $data['stockQuantityChart'] }}" alt="Gráfico de Quantidade de Produtos">
 
-        <h2>Gráfico sobre produtos com mais saídas</h2>
-        <img src="{{ $data['bestSellingProductsChart'] }}" alt="Gráfico de Produtos com Mais Saídas">
-
         <h2>Gráfico de vendas de cada mês</h2>
         <img src="{{ $data['monthlySalesChart'] }}" alt="Gráfico de Vendas Mensais">
     </body>
